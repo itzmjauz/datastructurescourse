d4b44d322df9b2b392dd808683e62383f939e633
itzmjauz <itzmjauz@gmail.com>
2015-09-16 23:59:14 +0200

    implement ternaryheapify

--- assignment-1/HeapSorter.java
+++ assignment-1/HeapSorter.java
@@ -28,16 +28,29 @@ class HeapSorter {
     int iterations = (toHeapify.length / 2);
 
     for(int x = iterations ; x >= 0 ; x--) {
-      int leftIndex = (x * 2) + 1;
-      int rightIndex = (x * 2) + 2;
+      for(int offset = 1 ; offset <= 2 ; offset++) {
+        int index = (x * 2) + offset;
 
-      if(leftIndex < toHeapify.length) {
-        if(toHeapify[leftIndex] > toHeapify[x]) {
-          swap(toHeapify, x, leftIndex);
+        if(index < toHeapify.length) {
+          if(toHeapify[index] > toHeapify[x]) {
+            swap(toHeapify, x, index);
+          }
         }
-        if(rightIndex < toHeapify.length) {
-          if(toHeapify[rightIndex] > toHeapify[x]) {
-            swap(toHeapify, x, rightIndex);
+      }
+    }
+    return toHeapify;
+  }
+
+  private int[] ternaryMaxHeapify(int[] toHeapify) {
+    int iterations = ((toHeapify.length + 1) / 3);
+
+    for(int x = iterations ; x >= 0 ; x--) {
+      for(int offset = 1 ; offset <= 3 ; offset++) {
+        int index = (x * 3) + offset;
+
+        if(index < toHeapify.length) {
+          if(toHeapify[index] > toHeapify[x]) {
+            swap(toHeapify, x, index);
           }
         }
       }
