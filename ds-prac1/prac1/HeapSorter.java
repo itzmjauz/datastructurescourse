a330f2e918d844e7c056589c0f8c5fc8e6c6d772
itzmjauz <itzmjauz@gmail.com>
2015-09-17 00:51:37 +0200

    make code compatible with given environment

--- ds-prac1/prac1/HeapSorter.java
+++ ds-prac1/prac1/HeapSorter.java
@@ -4,7 +4,7 @@ class HeapSorter {
 
   //start method called from Main, checks if we should handle the
   //toSorted array as binary or ternary
-  public int[] start(int[] toSort, boolean binaryHeap) {
+  static int[] start(int[] toSort, boolean binaryHeap) {
     int[] sorted = new int[toSort.length];
     int sortedIndex = toSort.length - 1;
     int mod = binaryHeap? 2 : 3; // binary or ternary ,, mod 2 | 3
@@ -19,7 +19,7 @@ class HeapSorter {
     return sorted;
   }
 
-  private int[] removeRoot(int[] toRemoveFrom) {
+  private static int[] removeRoot(int[] toRemoveFrom) {
     int[] result = new int[toRemoveFrom.length - 1];
 
     for(int x = 0 ; x < result.length ; x++) {
@@ -30,14 +30,14 @@ class HeapSorter {
   }
 
   // edits array ~ swaps the two given indexes
-  private void swap(int[] array, int index1, int index2) {
+  private static void swap(int[] array, int index1, int index2) {
     int x = array[index1];
     array[index1] = array[index2];
     array[index2] = x;
   }
 
   // maxheapifies the given array using swap() (binary)
-  private int[] maxHeapify(int[] toHeapify, int mod) {
+  private static int[] maxHeapify(int[] toHeapify, int mod) {
     int iterations = ((toHeapify.length + 1) / mod);
 
     for(int x = iterations ; x >= 0 ; x--) {
