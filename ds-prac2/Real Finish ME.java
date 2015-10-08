eb2f975b73ab2e5487a73a650c376ec579f268b0
Gallactide <mrdgall@gmail.com>
2015-10-08 21:03:50 +0200

    Code

--- ds-prac2/Real Finish ME.java
+++ ds-prac2/Real Finish ME.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 class Untitled {
 	
 	private static int getK(int size_x, int size_y) {
 
 		int size = size_y;
 		if (size_y<size_x) {
-			size = sizex;
+			size = size_x;
 		}
 	
 		//int k = (size_y* + size_x^2)^0.5;
@@ -13,20 +15,28 @@ class Untitled {
 	}
 		
 	private static int[] singleRow(int transformX, int transformY, int x, int y, int[][] input) {
-		int[] output = new int[size];
-		int counter = 0;
 		
 		int size_x = input[0].length;
 		int size_y = input.length;
 		
 		int k = getK(size_x-x, size_y-y);
+		System.out.println(k);
+		
+		int[] output = new int[getK(size_x-x, size_y-y)];
+		int counter = 0;
 		
 		for (int i = 0; i < k; i++) {
-			x+=transformX;
-			y+=transformY;
 			
+			System.out.println(""+x+","+y);
+		
 			output[counter] = input[y][x];
 			counter+=1;
+			
+			x+=transformX;
+			y+=transformY;
+			if(x<0 || y<0){
+				break;
+			}
 		}
 		
 		return output;
@@ -44,14 +54,17 @@ class Untitled {
 		
 		
 		for (int i = 0; i < input.length; i++) {
+			System.out.println("START ROW:"+x+","+y);
 			int[] row =  singleRow(transformX, transformY, x, y, input);
 			output[count] = row;
 			count+=1;
 			y+=1;
 		}
 		
-		y = 0;
-		for (int i = 0; i < input[0].length; i++) {
+		y = input.length-1;
+		x = 1;
+		for (int i = 0; i < input[0].length-1; i++) {
+			System.out.println("START ROW:"+x+","+y);
 			int[] row =  singleRow(transformX, transformY, x, y, input);
 			output[count] = row;
 			x+=1;
@@ -70,6 +83,11 @@ class Untitled {
 		int[] horiz = {1,0};
 		
 		int[][] rows = getRows(antiD[0], antiD[1], m);
+		
+		for (int i = 0; i < rows.length; i++) {
+			System.out.println(Arrays.toString(rows[i]));
+		}
+		
 		
 		//KADANE'S ALGORITHM + COMPARRISON
 		
